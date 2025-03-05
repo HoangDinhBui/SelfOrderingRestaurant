b46a63cfb4bc0cc08f09a88d58bc868cc9a803ff
HoangBD <buidinhhoang1910@gmail.com>
2025-03-05 13:48:47 +0700

    Initialize self-ordering Restaurant project

---
+++ src/main/java/com/example/SelfOrderingRestaurant/SelfOrderingRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example.SelfOrderingRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfOrderingRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfOrderingRestaurantApplication.class, args);
+	}
+
+}
