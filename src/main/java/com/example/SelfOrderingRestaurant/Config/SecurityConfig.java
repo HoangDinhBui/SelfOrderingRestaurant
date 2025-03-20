d2fc3a8c8ec35dee37685da545313077e304854d
MinhTu <minhtu06041@gmail.com>
2025-03-20 07:32:13 +0700

    feat: customers api

--- src/main/java/com/example/SelfOrderingRestaurant/Config/SecurityConfig.java
+++ src/main/java/com/example/SelfOrderingRestaurant/Config/SecurityConfig.java
@@ -26,7 +26,8 @@ public class SecurityConfig {
                                          "/error",
                                          "/api/tables/**",
                                          "/api/staff/**",
-                                        "/api/notifications/**"
+                                        "/api/notifications/**",
+                                        "/api/customers/**"
                         ).permitAll() // API order không cần auth
                         .anyRequest().authenticated() // Các API khác vẫn yêu cầu login
                 )
