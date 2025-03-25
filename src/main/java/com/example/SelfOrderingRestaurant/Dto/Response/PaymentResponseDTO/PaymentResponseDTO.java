bebde03afb094a7edfee82b2b4ecb39072855b1c
HoangBD <buidinhhoang1910@gmail.com>
2025-03-25 12:04:09 +0700

    Fix: The feature Get transaction status is false, some attribute is null after pay the bill

--- src/main/java/com/example/SelfOrderingRestaurant/Dto/Response/PaymentResponseDTO/PaymentResponseDTO.java
+++ src/main/java/com/example/SelfOrderingRestaurant/Dto/Response/PaymentResponseDTO/PaymentResponseDTO.java
@@ -1,14 +1,9 @@
 package com.example.SelfOrderingRestaurant.Dto.Response.PaymentResponseDTO;
 
-import com.example.SelfOrderingRestaurant.Enum.PaymentMethod;
-import com.example.SelfOrderingRestaurant.Enum.PaymentStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -17,4 +12,6 @@ import java.time.LocalDateTime;
 public class PaymentResponseDTO {
     private String paymentUrl;
     private String message;
+    private String transactionStatus;
+    private String responseCode;
 }
