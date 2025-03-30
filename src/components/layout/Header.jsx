c0a3d6151ef428966755edf79547d4b91976498a
MinhTu <minhtu06041@gmail.com>
2025-03-30 12:26:47 +0700

    Initial commit

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,15 @@
+// filepath: c:\MinhTu\selforderingrestaurant\src\components\layout\Header.jsx
+const Header = () => {
+  return (
+    <header className="text-left py-2">
+      <h1 className="text-xl font-bold italic" style={{ fontFamily: "'Dancing Script', cursive" }}>
+        Bon Appétit
+      </h1>
+      <p className="text-base text-gray-600">
+        450 Le Van Viet Street, Tang Nhon Phu A Ward, District 9
+      </p>
+    </header>
+  );
+};
+
+export default Header;
