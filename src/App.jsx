ab2b9e54efdd2e4f69b11d3221ea936909639b72
MinhTu <minhtu06041@gmail.com>
2025-03-31 22:30:16 +0700

    Feat:Note

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home";
 import Menu from './pages/Menu/Menu';
 import Order from './pages/Order/Order'; // Import trang Order
 import ViewItem from "./pages/ViewItem/ViewItem";
+import Note from "./pages/Note/Note";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/menu" element={<Menu />} />
         <Route path="/order" element={<Order />} /> {/* Thêm route cho trang Order */}
         <Route path="/view/:id" element={<ViewItem />} />
+        <Route path="/note/:id" element={<Note />} />
       </Routes>
     </Router>
   );
