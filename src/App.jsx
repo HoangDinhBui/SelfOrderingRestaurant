171753a82ea60b952605943fe187daea9ff8243e
MinhTu <minhtu06041@gmail.com>
2025-04-03 16:50:13 +0700

    Feat: login

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Customer/Home/Home";
-import Menu from './pages/Customer/Menu/Menu';
-import Order from './pages/Customer/Order/Order'; // Import trang Order
-import ViewItem from "./pages/Customer/ViewItem/ViewItem";
-import Note from "./pages/Customer/Note/Note";
-import Payment from "./pages/Customer/PayMent/Payment";
-import Evaluate from "./pages/Customer/Evaluate/Evaluate";
+// import Home from "./pages/Customer/Home/Home";
+// import Menu from './pages/Customer/Menu/Menu';
+// import Order from './pages/Customer/Order/Order'; // Import trang Order
+// import ViewItem from "./pages/Customer/ViewItem/ViewItem";
+// import Note from "./pages/Customer/Note/Note";
+// import Payment from "./pages/Customer/PayMent/Payment";
+// import Evaluate from "./pages/Customer/Evaluate/Evaluate";
+import Login from "./pages/Staff/Login";
 
 function App() {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
-          <Route path="/order" element={<Order />} /> {/* Thêm route cho trang Order */}
-          <Route path="/view/:id" element={<ViewItem />} />
+          <Route path="/order" element={<Order />} /> 
+          <Route path="/view/:id" element={<ViewItem />} /> 
           <Route path="/note/:id" element={<Note />} />
           <Route path="/payment" element={<Payment />} />
-          <Route path="/evaluate" element={<Evaluate />} />
+          <Route path="/evaluate" element={<Evaluate />} />  */}
+          <Route path="/login" element={<Login />} /> 
 
         
 
