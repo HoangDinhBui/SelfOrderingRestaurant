c87e1a524d4b48ab2529fdba482f050b98622f5a
MY DUNG <dungne812@gmail.com>
2025-04-05 16:21:09 +0700

    Feat: MenuBar Component

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Customer/Home/Home";
+import Home from "./pages/Customer/Home/Home";
 // import Menu from './pages/Customer/Menu/Menu';
 // import Order from './pages/Customer/Order/Order'; // Import trang Order
 // import ViewItem from "./pages/Customer/ViewItem/ViewItem";
@@ -8,20 +8,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Evaluate from "./pages/Customer/Evaluate/Evaluate";
 import Login from "./pages/Staff/Login/Login";
 import StaffInformation from "./pages/Staff/StaffInformation/StaffInformation";
+import DishManagement from "./pages/Admin/DishManagement";
 
 function App() {
   return (
     <Router>
         <Routes>
           {/* <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
+          {/* <Route path="/menu" element={<Menu />} />
           <Route path="/order" element={<Order />} /> 
           <Route path="/view/:id" element={<ViewItem />} /> 
           <Route path="/note/:id" element={<Note />} />
           <Route path="/payment" element={<Payment />} />
           <Route path="/evaluate" element={<Evaluate />} />  */}
-          <Route path="/login" element={<Login />} /> 
-          <Route path="/staff-information" element={<StaffInformation />} />
+          {/* <Route path="/login" element={<Login />} /> 
+          <Route path="/" element={<StaffInformation />} /> */}
+          <Route path="/" element={<DishManagement/>}/>
 
         
 
