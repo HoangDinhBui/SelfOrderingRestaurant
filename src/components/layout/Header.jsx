753046497d36e2bcab1faef2c110d86e80449dc2
MY DUNG <dungne812@gmail.com>
2025-04-29 17:15:04 +0700

    Feat: Fix customer's home

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,11 +2,9 @@
 const Header = () => {
   return (
     <div className="flex justify-center">
-      <header className="text-center py-2 w-full max-w-md">  {/* Giới hạn chiều rộng */}
-        <h1 className="text-xl font-bold italic" style={{ fontFamily: "'Dancing Script', cursive" }}>
-          Bon Appétit
-        </h1>
-        <p className="text-base text-gray-600">
+      <header className="text-center py-2 w-90 max-w-md">  {/* Giới hạn chiều rộng */}
+        <img src="/src/assets/img/logohome.png" alt="Logo" className="w-1/2 mx-auto mb-4" />
+        <p className="text-sm text-gray-600">
           450 Le Van Viet Street, Tang Nhon Phu A Ward, District 9
         </p>
       </header>
