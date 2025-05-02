5d6f376f70c9f8d64c5b7cd7f5b1caf6860a4cb5
HoangBD <buidinhhoang1910@gmail.com>
2025-05-02 17:50:04 +0700

    Feat: Update more function for notification API

--- frontend/frontend/src/main/java/com/example/SelfOrderingRestaurant/Repository/StaffRepository.java
+++ frontend/frontend/src/main/java/com/example/SelfOrderingRestaurant/Repository/StaffRepository.java
@@ -2,10 +2,14 @@ package com.example.SelfOrderingRestaurant.Repository;
 
 import com.example.SelfOrderingRestaurant.Entity.Staff;
 import com.example.SelfOrderingRestaurant.Entity.User;
+import com.example.SelfOrderingRestaurant.Enum.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StaffRepository extends JpaRepository<Staff, Integer> {
     Staff findByUser(User user);
+    List<Staff> findAllByStatus(UserStatus status);
 }
